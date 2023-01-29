ae3a72fc3dc42d3e845d12b24c3dc554ad194b7d
Andreas Hallberg <andreas.hallberg@infor.com>
2023-01-29 17:18:45 +0100

    montage2

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const observer = new IntersectionObserver((entries) => {
-    for (const entry of entries) {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("active");
-        }
-        else {
-            // entry.target.classList.remove("active");
-        }
-    }
-}, { threshold: 0.1 });
-document
-    .querySelectorAll(".fade-in")
-    .forEach((element) => observer.observe(element));
 function startGallery(gallery) {
     setInterval(showNext, 5000);
     function showNext() {
--- script.ts
+++ script.ts
@@ -1,19 +1,3 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      } else {
-        // entry.target.classList.remove("active");
-      }
-    }
-  },
-  { threshold: 0.1 }
-);
-document
-  .querySelectorAll(".fade-in")
-  .forEach((element) => observer.observe(element));
-
 function startGallery(gallery: HTMLElement) {
   setInterval(showNext, 5000);
 
